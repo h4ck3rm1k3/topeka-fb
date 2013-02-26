0c8cd7fc2e4233f0ae033f438220460f977c9ce1
James Michael DuPont <JamesMikeDuPont@gmail.com>
2013-02-25 23:17:10 -0600

    likes

--- exampleapp.py
+++ exampleapp.py
@@ -288,7 +288,9 @@ def likes():
         newid = -1
         local2=[]
 
-        local= get_all('me/likes', {'access_token': access_token})
+        local= get_all('me/likes', {'access_token': access_token
+                                    'fields' :  'id,name,location,website'
+                                    })
         newid = -1
         for d in local:
             d['osmid'] = newid
